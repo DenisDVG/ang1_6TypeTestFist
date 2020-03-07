8efc0014b704653885ed60c9d6acc4e0db2ac2a4
DenisGromenkoV <156789qw@gmail.com>
2020-03-07 13:46:59 +0200

    count home

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,6 +1,19 @@
 class HomeController implements ng.IController {
     welcome:string='hello ng';
+    count: number;
+    addCount(e){
+        e.stopPropagation();
+        this.count++;
+    }
+    clearCount(e){
+        // e.stopPropagation(); этот метод вынесен сразу в темплейт
+        this.count = 0;
+    }
+    clickDivCount(){
+        this.count++;
+    }
     constructor() {
+        this.count = 0;
     }
 }
 
