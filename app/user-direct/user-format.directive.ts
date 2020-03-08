b020b89c93ca58e906637af87f90b4551231a59c
DenisGromenkoV <156789qw@gmail.com>
2020-03-08 18:18:37 +0200

    dir link

--- app/user-direct/user-format.directive.ts
+++ app/user-direct/user-format.directive.ts
@@ -2,9 +2,18 @@ export class UserFormat implements ng.IDirective
 {
     static NAME: string = 'userFormat';
     restrict = 'E'; // by defalte
-    template = 'Templ:  {{ ::customerInfo.name }}  <input ng-model="customerInfo.name"></button>'; // привязано к имени переменной во внешн скоупе
+    template = 'Templ: <span>{{ ::customerInfo.name }}</span>  <input ng-model="customerInfo.name"></button>'; // привязано к имени переменной во внешн скоупе
     scope = {
         customerInfo: '=info'
+    };v
+
+    link = (scope: ng.IScope,
+        element: ng.IAugmentedJQuery,
+        attrs: ng.IAttributes, ) => {
+        var spanElem = element.find('span');
+        spanElem[0].addEventListener("click", function () {
+            this.remove();
+        });
     };
 
     static factory(): ng.IDirectiveFactory {
